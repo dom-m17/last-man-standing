1b02f95e0b62ff38516b9e19c5e6cc4c39e30b9c
dom-m17 <dominicmaynard00@gmail.com>
2025-02-08 20:32:20 +0000

    more experimenting with backend server

--- backend/main.go
+++ backend/main.go
@@ -11,26 +11,81 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	_ = godotenv.Load()
+var db = make(map[string]string)
 
-	getAllClubs()
-
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
 	r := gin.Default()
-	r.GET("/clubs", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	// Ping test
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, getAllClubs())
 	})
-	r.Run()
+
+	// Get user value
+	r.GET("/user/:name", func(c *gin.Context) {
+		user := c.Params.ByName("name")
+		value, ok := db[user]
+		if ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+		}
+	})
+
+	// Authorized group (uses gin.BasicAuth() middleware)
+	// Same than:
+	// authorized := r.Group("/")
+	// authorized.Use(gin.BasicAuth(gin.Credentials{
+	//	  "foo":  "bar",
+	//	  "manu": "123",
+	//}))
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"foo":  "bar", // user:foo password:bar
+		"manu": "123", // user:manu password:123
+	}))
+
+	/* example curl for /admin with basicauth header
+	   Zm9vOmJhcg== is base64("foo:bar")
+
+		curl -X POST \
+	  	http://localhost:8080/admin \
+	  	-H 'authorization: Basic Zm9vOmJhcg==' \
+	  	-H 'content-type: application/json' \
+	  	-d '{"value":"bar"}'
+	*/
+	authorized.POST("admin", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+
+		// Parse JSON
+		var json struct {
+			Value string `json:"value" binding:"required"`
+		}
+
+		if c.Bind(&json) == nil {
+			db[user] = json.Value
+			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		}
+	})
+
+	return r
 }
 
-func getAllClubs() {
+func main() {
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
+
+func getAllClubs() string {
+	godotenv.Load()
+
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", "http://api.football-data.org/v4/competitions/PL/teams", nil)
 	req.Header.Set("X-Auth-Token", os.Getenv("FOOTBALL_DATA_API_KEY"))
-	response, err := client.Do(req)
 
+	response, err := client.Do(req)
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
@@ -40,5 +95,6 @@ func getAllClubs() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(responseData))
+
+	return string(responseData)
 }
