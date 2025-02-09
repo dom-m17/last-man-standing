a080d7fac140aeb8d50e09d7ac46333c6a89304a
dom-m17 <dominicmaynard00@gmail.com>
2025-02-09 11:54:13 +0000

    getting team names on endpoint

--- backend/main.go
+++ backend/main.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"reflect"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -23,52 +22,6 @@ func setupRouter() *gin.Engine {
 	// Ping test
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, getAllClubs())
-	})
-
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
 	})
 
 	return r
@@ -97,15 +50,25 @@ func getAllClubs() interface{} {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer response.Body.Close()
 
 	var teamsAsJSON map[string]interface{}
 	json.Unmarshal(responseData, &teamsAsJSON)
 
-	// I don't know how to get the values out of here :(
-	teams := teamsAsJSON["teams"]
+	teams, ok := teamsAsJSON["teams"].([]interface{})
+	if !ok {
+		log.Fatal("Error parsing teams data")
+	}
 
-	fmt.Println(reflect.TypeOf(teamsAsJSON))
-	fmt.Println(reflect.TypeOf(teams))
+	var teamNames []string
+	for _, team := range teams {
+		teamMap, ok := team.(map[string]interface{})
+		if ok {
+			if name, exists := teamMap["name"].(string); exists {
+				teamNames = append(teamNames, name)
+			}
+		}
+	}
 
-	return teams
+	return teamNames
 }
