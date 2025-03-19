98c21fba54b9ceb6b49042f7c3571f4843506fda
Dominic Maynard <dominicmaynard00@gmail.com>
2025-03-19 21:28:43 +0000

    experimenting with shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
 
   return (
     <>
       <header className="App-header">Last Man Standing</header>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
     </>
   )
 }
