160cabb0c0dddd7e5192c29099c32c62dd5397f6
Dominic Maynard <dominic.maynard@withwise.com>
2025-07-19 14:39:35 +0100

    query now returns results

--- frontend/src/components/MatchList.tsx
+++ frontend/src/components/MatchList.tsx
@@ -2,28 +2,32 @@ import Match from "./Match";
 import styles from "./MatchList.module.css";
 
 async function getMatchesByMatchday(matchday: number) {
-    try {
-        let result = await fetch("http://localhost/8080/query", {
-            method: "GET",
-            headers: {
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                query: `
-                    getMatchesByMatchday
-                `
-            })
-        })
-        let matches = await result.json()
-        console.log(matches.homeTeam)
-    } catch (err) {
-        console.log("fail")
-    }
+  try {
+    let result = await fetch("http://localhost:8080/query", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            getMatchesByMatchday(input: ${matchday}) {
+              id
+            }
+          }
+        `,
+      }),
+    });
+    let matches = await result.json();
+    console.log(matches);
+    return matches;
+  } catch (err) {
+    console.error("Error fetching matches:", err);
+  }
 }
 
 export default function MatchList() {
-
-  getMatchesByMatchday(1)
+  getMatchesByMatchday(1);
 
   return (
     <div>
